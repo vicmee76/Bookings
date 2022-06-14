660c4e102f6a5d380b5a9164c755ab199248723b
Victor Nwogu <vicmee76@hotmail.com>
2022-06-14 23:29:49 +0100

    -- readded

---
+++ BookingsTest.Test/MockData/MockUsers.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using Bookings.Models.DB;
+using System.Collections.Generic;
+
+namespace BookingsTest.Test.MockData
+{
+    public class MockUsers
+    {
+        public List<User> getUsers(int generate)
+        {
+            var users = new Faker<User>()
+                .RuleFor(x => x.UsersId, x => x.Random.Int())
+                .RuleFor(x => x.FirstName, x => x.Person.FirstName)
+                .RuleFor(x => x.LastName, x => x.Person.LastName)
+                .RuleFor(x => x.Email, x => x.Person.Email)
+                .RuleFor(x => x.Phone, x => x.Person.Phone)
+                .RuleFor(x => x.Address, x => x.Address.StreetAddress());
+
+            return users.Generate(generate);
+        }
+    }
+}
---
+++ BookingsTest.Test/Services/UserServices/UserServiceTest.cs
@@ -0,0 +1,175 @@
+﻿using Bookings.Models.DB;
+using Bookings.Services.UserServices;
+using BookingsTest.Test.MockData;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BookingsTest.Test.Services.Userservices
+{
+
+    public class UserServiceTest : IDisposable
+    {
+
+        private readonly UserService _sut;
+        private BOOKINGS_DBContext _mockContext;
+        private readonly MockUsers _mockUsers = new();
+
+        public UserServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<BOOKINGS_DBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _mockContext = new(options);
+            _sut = new(_mockContext);
+        }
+
+
+
+        public void Dispose()
+        {
+            _mockContext.Database.EnsureDeleted();
+            _mockContext.Dispose();
+        }
+
+
+        #region Test for GetAllUsers
+
+        [Fact]
+        public void GetAllUsers_ShouldReturnAListOfUsers()
+        {
+            //Arrange
+            var data = _mockUsers.getUsers(5);
+            _mockContext.Users.AddRange(data);
+            _mockContext.SaveChanges();
+
+            //Act
+            var result =  _sut.GetAllUsers();
+
+            //Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeEquivalentTo(data);
+            result.Should().BeEquivalentTo(data, x => x.ExcludingMissingMembers());
+        }
+
+
+        [Fact]
+        public void GetAllUsers_ShouldReturnTheCorrectCountOfUsers()
+        {
+            //Arrange
+            var data = _mockUsers.getUsers(5);
+            _mockContext.Users.AddRange(data);
+            _mockContext.SaveChanges();
+
+            //Act
+            var result = _sut.GetAllUsers();
+
+            //Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeEquivalentTo(data);
+            result.Should().BeEquivalentTo(data, x => x.ExcludingMissingMembers());
+            result.Should().HaveCount(data.Count);
+        }
+
+
+        [Fact]
+        public void GetAllUsers_ShouldReturnNullIfNoListOfUsersIsFound()
+        {
+            //Arrange
+            var data = _mockUsers.getUsers(0);
+            _mockContext.Users.AddRange(data);
+            _mockContext.SaveChanges();
+
+            //Act
+            var result = _sut.GetAllUsers();
+
+            //Assert
+            result.Should().BeEquivalentTo(data);
+            result.Should().BeEquivalentTo(data, x => x.ExcludingMissingMembers());
+            result.Should().BeNullOrEmpty();
+        }
+
+
+        #endregion
+
+
+
+        #region Test for GetUserById
+
+        [Theory]
+        [InlineData(0)]
+        public void GetUserById_ShouldReturnNullIfNoUserIdIsPassed(int id)
+        {
+            //Arrange
+            var data = _mockUsers.getUsers(0).FirstOrDefault();
+
+            //Act
+            var result = _sut.GetUserById(id);
+
+            //
+            result.Should().BeNull();
+            result.Should().BeEquivalentTo(data);
+        }
+
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void GetUserById_ShouldReturnNullIfNoUserWasFound(int id)
+        {
+            //Arrange
+            var data = _mockUsers.getUsers(0).FirstOrDefault();
+
+            //Act
+            var result = _sut.GetUserById(It.IsAny<int>());
+
+            //Assert
+            result.Should().BeNull();
+            result.Should().BeEquivalentTo(data);
+            id.Should().NotBe(0);
+            id.Should().BeOfType(typeof(int));
+        }
+
+
+
+       [Fact]
+        public void GetUserById_ShouldReturnTheUserWhenFound()
+        {
+            //Arrange
+            var data = _mockUsers.getUsers(1).FirstOrDefault();
+            _mockContext.Users.Add(data);
+            _mockContext.SaveChanges();
+
+            //Act
+            var result = _sut.GetUserById(data.UsersId);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(data);
+            result.Should().BeEquivalentTo(data, x => x.ComparingRecordsByValue().ComparingByMembers<IEnumerable<User>>());
+         
+        }
+
+        #endregion
+
+
+
+
+        //public async Task CreateUser_ShouldReturnNullIfUserModelIsEmpty()
+        //{
+        //    //Arrange
+        //    var data = _mockUsers.getUsers(0).FirstOrDefault();
+
+
+        //    //Act
+        //    var result = _sut.CreateUser(data);
+
+        //    //Assert
+        //}
+    }
+}
